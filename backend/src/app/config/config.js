35362e87b3594d33ea43fe3c929ee0b21229e4f8
undefined <undefined>
2023-10-18 10:46:34 +0200

    .env should be placed in backend dir

--- backend/src/app/config/config.js
+++ backend/src/app/config/config.js
@@ -1,6 +1,6 @@
 module.exports = (configPath) => {
     const dotenv = require('dotenv');
-    const envFile = configPath || process.cwd() + '/configuration/.env';
+    const envFile = configPath || process.cwd() + '/.env';
     dotenv.config({ path: envFile });
 }
 

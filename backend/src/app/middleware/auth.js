4188eb37326fe77e93002375954d6c7297ecb8db
JensWinkler91 <jens.winkler@gbes.de>
2023-11-07 11:47:23 +0100

    return after sending an error - otherwise the requested function would be triggered

--- backend/src/app/middleware/auth.js
+++ backend/src/app/middleware/auth.js
@@ -2,7 +2,7 @@ module.exports.apiKeyMiddleware = (app) => {
     app.use((req, res, next) => {
         const apiKey = req.get("x-api-key");
         if (apiKey !== process.env.API_KEY) {
-            res.status(403).send({error: {code: 403, message: "Forbidden"}});
+            return res.status(403).send({error: {code: 403, message: "Forbidden"}});
         }
 
         next();
